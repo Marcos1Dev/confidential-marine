727155403c5989ad8a730b33b0071e55f266748d
Marcos Guarnieri <marcosguarnieri3@gmail.com>
2024-06-15 20:21:19 -0300

    confidential-marine

---
+++ src/js/index.js
@@ -0,0 +1,44 @@
+alert ("Confirme sua identificação como um oficial da marinha para prosseguir!")
+
+const btnAvancar = document.getElementById("btn-avancar");
+
+const btnVoltar = document.getElementById("btn-voltar");
+let cartaoAtual = 0;
+const cartoes = document.querySelectorAll(".cartao");
+
+btnAvancar.addEventListener("click", function () {
+    const ehUltimoCartao = cartaoAtual === cartoes.length - 1; 
+if (ehUltimoCartao) return;
+
+    esconderCartaoSelecionado();
+
+
+    cartaoAtual++;
+
+    mostrarCartao();
+
+});
+
+
+btnVoltar.addEventListener("click", function () {
+    const ehPrimeiroCartao = cartaoAtual === 0;
+    if(ehPrimeiroCartao) return;
+
+    esconderCartaoSelecionado();
+    
+
+    cartaoAtual--;
+    mostrarCartao();
+    });
+
+
+
+function mostrarCartao() {
+    cartoes[cartaoAtual].classList.add("selecionado");
+}
+
+function esconderCartaoSelecionado() {
+    const cartaoSelecionado = document.querySelector(".selecionado");
+    cartaoSelecionado.classList.remove("selecionado");
+}
+
